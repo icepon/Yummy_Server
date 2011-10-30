ff3aff20e877e58c78cffd9d8e9794ec6008e2e5
unknown <Vee@Vee-PC.(none)>
2011-10-30 21:28:32 +0700

    Updated

--- Yummy_Server/sourcecode/Yummy/Yummy/Controllers/HomeController.cs
+++ Yummy_Server/sourcecode/Yummy/Yummy/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Yummy.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "YUMMY";
             return View();
         }
 
