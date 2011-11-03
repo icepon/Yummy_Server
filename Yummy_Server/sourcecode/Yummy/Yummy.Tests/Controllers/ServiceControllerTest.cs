2a77203ada43d9b729d5280992b0c4d48f5d9008
unknown <Vee@Vee-PC.(none)>
2011-11-03 16:22:17 +0700

    Added resources

---
+++ Yummy_Server/sourcecode/Yummy/Yummy.Tests/Controllers/ServiceControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yummy;
+using Yummy.Controllers;
+
+namespace Yummy.Tests.Controllers
+{
+    [TestClass()]
+    public class ServiceControllerTest
+    {
+        [TestMethod]
+        public void It_should_return_JsonResult_when_UploadPhoto()
+        {
+            ServiceController serviceController = new ServiceController();
+            ActionResult result = serviceController.UploadPhoto(@"C:\Users\Vee\Documents\Visual Studio 2010\Projects\Yummy\Yummy_Server\sourcecode\Yummy\Yummy\Content\images\coffee_cup.png");
+            Assert.IsInstanceOfType(result, typeof(JsonResult));
+        }
+    }
+}
---
+++ Yummy_Server/sourcecode/Yummy/Yummy/Controllers/ServiceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlickrNet;
+
+namespace Yummy.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public ServiceController()
+        {
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UploadPhoto(string fileName)
+        {
+            return new JsonResult() { Data = "Upload Successed" };
+        }
+    }
+}
